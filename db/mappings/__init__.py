843fc0aeb27805e23cc16223d317b2ff9af38ed6
ting zhang <thetingzhang@gmail.com>
2021-01-29 18:08:52 -0500

    [Fix] centralize pytest setup

--- db/mappings/__init__.py
+++ db/mappings/__init__.py
@@ -1,23 +1,13 @@
 from peewee import SqliteDatabase
 
 from db.mappings.base import db_proxy
-from db.mappings.article import Article
-from db.mappings.model import Model
-from db.mappings.recommendation import Rec
 from lib.db import db as PostgresDB
 from lib.config import config
-
-MAPPINGS = (Model, Article, Rec)
-
-
-def recreate_tables(db):
-    db.drop_tables(MAPPINGS)
-    db.create_tables(MAPPINGS)
 
 
 if config.get("TEST_DB"):
     database = SqliteDatabase("/tmp/local.db")
-    db_proxy.initialize(database)
-    recreate_tables(database)
 else:
-    db_proxy.initialize(PostgresDB)
+    database = PostgresDB
+
+db_proxy.initialize(database)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,21 @@
 from lib.config import config
+from db.mappings.model import Model
+from db.mappings.article import Article
+from db.mappings.recommendation import Rec
+from db.mappings import database
+
+
+MAPPINGS = (Model, Article, Rec)
+
+
+def recreate_tables(db):
+    db.drop_tables(MAPPINGS)
+    db.create_tables(MAPPINGS)
 
 
 def pytest_configure():
     config._config["TEST_DB"] = True
+    recreate_tables(database)
 
 
 def pytest_unconfigure():
