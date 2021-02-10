d62de4d9f9c667d72497c24e4f48c97a9aa6bbca
ting zhang <thetingzhang@gmail.com>
2021-02-10 18:06:08 -0500

    [Add] tests for error cases

--- tests/handlers/test_model_handler.py
+++ tests/handlers/test_model_handler.py
@@ -5,6 +5,7 @@ import tornado.testing
 from tests.factories.model import ModelFactory
 from tests.base import BaseTest
 from db.mappings.model import Type, Status
+from lib.config import config
 
 
 class TestModelHandler(BaseTest):
@@ -45,3 +46,38 @@ class TestModelHandler(BaseTest):
 
         assert len(results["results"]) == 1
         assert results["results"][0]["type"] == Type.ARTICLE.value
+
+    @tornado.testing.gen_test
+    async def test_patch__no_admin_token__throws_error(self):
+        response = await self.http_client.fetch(
+            self.get_url(f"{self._endpoint}/1/make_current"),
+            method="PATCH",
+            raise_error=False,
+            allow_nonstandard_methods=True,
+        )
+
+        assert response.code == 401
+
+    @tornado.testing.gen_test
+    async def test_patch__wrong_admin_token__throws_error(self):
+        response = await self.http_client.fetch(
+            self.get_url(f"{self._endpoint}/1/make_current"),
+            method="PATCH",
+            raise_error=False,
+            allow_nonstandard_methods=True,
+            headers={"Authorization": "bad-auth"},
+        )
+
+        assert response.code == 403
+
+    @tornado.testing.gen_test
+    async def test_patch__missing_resource__throws_error(self):
+        response = await self.http_client.fetch(
+            self.get_url(f"{self._endpoint}/1/make_current"),
+            method="PATCH",
+            raise_error=False,
+            allow_nonstandard_methods=True,
+            headers={"Authorization": config.get("ADMIN_TOKEN")},
+        )
+
+        assert response.code == 404
