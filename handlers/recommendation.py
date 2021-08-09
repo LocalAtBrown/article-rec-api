fb36c276a307bb874ac17a9c52f33bef39e36c3d
ting zhang <thetingzhang@gmail.com>
2021-08-09 13:37:21 -0400

    [Change] move validation to own func

--- handlers/recommendation.py
+++ handlers/recommendation.py
@@ -1,9 +1,10 @@
 import operator
-import tornado.web
-
 from datetime import datetime, timedelta
 from functools import reduce
 from random import randint
+from typing import Dict
+
+import tornado.web
 
 from db.helpers import retry_rollback
 from db.mappings.recommendation import Rec
@@ -25,7 +26,10 @@ class DefaultRecs:
             query = (
                 Rec.select()
                 .join(Model, on=(Model.id == Rec.model))
-                .where((Model.type == cls.DEFAULT_TYPE) & (Model.status == Status.CURRENT.value))
+                .where(
+                    (Model.type == cls.DEFAULT_TYPE)
+                    & (Model.status == Status.CURRENT.value)
+                )
                 .order_by(Rec.score.desc())
             )
             cls._recs = [x.to_dict() for x in query]
@@ -56,48 +60,23 @@ class RecHandler(APIHandler):
         clauses = []
 
         if "source_entity_id" in filters:
-            clauses.append((self.mapping.source_entity_id == filters["source_entity_id"]))
-            try:
-                int(filters["source_entity_id"])
-            except:
-                raise tornado.web.HTTPError(
-                    status_code=400,
-                    log_message=f"Invalid input syntax for source_entity_id (int): {filters['source_entity_id']}"
-                )
+            clauses.append(
+                (self.mapping.source_entity_id == filters["source_entity_id"])
+            )
 
         if "exclude" in filters:
-            query = query.join(Article, on=(Article.id == self.mapping.recommended_article)).where(
-                (Article.external_id.not_in(filters["exclude"].split(",")))
-            )
-            for exclude in filters["exclude"].split(","):
-                try:
-                    int(exclude)
-                except:
-                    raise tornado.web.HTTPError(
-                        status_code=400,
-                        log_message=f"Invalid input syntax for source_entity_id (List[int]): {filters['exclude']}"
-                    )
+            query = query.join(
+                Article, on=(Article.id == self.mapping.recommended_article)
+            ).where((Article.external_id.not_in(filters["exclude"].split(","))))
 
         if "model_id" in filters:
             clauses.append((self.mapping.model_id == filters["model_id"]))
-            try:
-                int(filters["model_id"])
-            except:
-                raise tornado.web.HTTPError(
-                    status_code=400,
-                    log_message=f"Invalid input syntax for model_id (int): {filters['model_id']}"
-                )
+
         elif "model_type" in filters:
             query = query.join(Model, on=(Model.id == self.mapping.model)).where(
-                (Model.type == filters["model_type"]) & (Model.status == Status.CURRENT.value)
+                (Model.type == filters["model_type"])
+                & (Model.status == Status.CURRENT.value)
             )
-            try:
-                str(filters["model_type"])
-            except:
-                raise tornado.web.HTTPError(
-                    status_code=400,
-                    log_message=f"Invalid input syntax for model_type (str): {filters['model_type']}"
-                )
 
         if len(clauses):
             conditional = reduce(operator.and_, clauses)
@@ -105,9 +84,50 @@ class RecHandler(APIHandler):
 
         return query
 
+    def validate_filters(self, **filters) -> Dict[str, str]:
+        error_msgs = {}
+
+        if "source_entity_id" in filters:
+            try:
+                int(filters["source_entity_id"])
+            except:
+                error_msgs[
+                    "source_entity_id"
+                ] = f"Invalid input syntax (int): {filters['source_entity_id']}"
+
+        if "exclude" in filters:
+            for exclude in filters["exclude"].split(","):
+                try:
+                    int(exclude)
+                except:
+                    error_msgs[
+                        "exclude"
+                    ] = f"Invalid input syntax (List[int]): {filters['exclude']}"
+
+        if "model_id" in filters:
+            try:
+                int(filters["model_id"])
+            except:
+                error_msgs[
+                    "model_id"
+                ] = f"Invalid input syntax (int): {filters['model_id']}"
+
+        elif "model_type" in filters:
+            try:
+                str(filters["model_type"])
+            except:
+                error_msgs[
+                    "model_type"
+                ] = f"Invalid input syntax (str): {filters['model_type']}"
+
+        return error_msgs
+
     @retry_rollback
     async def get(self):
         filters = self.get_arguments()
+        validation_errors = self.validate_filters(**filters)
+        if validation_errors:
+            raise tornado.web.HTTPError(status_code=400, log_message=validation_errors)
         query = self.mapping.select()
         query = self.apply_conditions(query, **filters)
         query = self.apply_sort(query, **filters)
