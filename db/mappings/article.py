fc2d4b63db4ddc7265f18f183d5e21f65fe2c302
ting zhang <thetingzhang@gmail.com>
2021-11-30 10:36:13 -0500

    Revert "[Change] external id field to text"
    This reverts commit 6c15bde823fa0696d58ce24b9d2bd6d89a2d082c.

--- db/mappings/article.py
+++ db/mappings/article.py
@@ -9,8 +9,9 @@ class Article(BaseMapping):
     class Meta:
         table_name = "article"
 
-    external_id = TextField(null=False, default="")
+    external_id = IntegerField(null=False)
     title = TextField(null=False, default="")
     path = TextField(null=False, default="")
     published_at = DateTimeField(null=True)
     site = TextField(null=False)
+
