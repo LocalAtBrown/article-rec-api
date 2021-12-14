d7f21733086bff857a66adfba226dd92ecc85374
ting zhang <thetingzhang@gmail.com>
2021-12-14 13:32:52 -0500

    [Add] limit to 500 articles

--- db/helpers.py
+++ db/helpers.py
@@ -7,6 +7,10 @@ from peewee import Expression, InterfaceError
 from db.mappings.base import BaseMapping, tzaware_now
 from db.mappings.article import Article
 from lib.db import db
+from lib.config import config
+
+
+MAX_PAGE_SIZE = config.get("MAX_PAGE_SIZE")
 
 
 def create_resource(mapping_class: BaseMapping, **params: dict) -> int:
@@ -31,8 +35,10 @@ def update_resources(
 def get_articles_by_external_ids(site: str, external_ids: List[str]) -> List[dict]:
     res = (
         Article.select()
-        .where((Article.site == site) & Article.external_id.in_(list(external_ids)))
+        .where((Article.site == site))
+        .where(Article.external_id.in_(list(external_ids)))
         .order_by(Article.published_at.desc())
+        .limit(MAX_PAGE_SIZE)
     )
     if res:
         return [r.to_dict() for r in res]
