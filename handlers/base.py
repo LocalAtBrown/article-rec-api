bc4a8c696f48df4371f90172ec5832b5de905a6b
ting zhang <thetingzhang@gmail.com>
2022-02-03 14:28:21 -0500

    [update] sort by to work with null vals

--- handlers/base.py
+++ handlers/base.py
@@ -140,17 +140,18 @@ class APIHandler(BaseHandler):
 
     def apply_sort(self, query, **filters):
         DEFAULT_ORDER_BY = "desc"
-        if "sort_by" in filters:
+
+        sort_by_field = None
+        if filters.get("sort_by"):
             sort_by_field = getattr(self.mapping, filters["sort_by"], None)
-            if not sort_by_field:
-                return query
-            default_order_rule = getattr(sort_by_field, DEFAULT_ORDER_BY)
-            order_by_rule = getattr(
-                sort_by_field,
-                filters.get("order_by", DEFAULT_ORDER_BY),
-                default_order_rule,
-            )
-            query = query.order_by(order_by_rule())
+        if not sort_by_field:
+            return query
+
+        default_order_rule = getattr(sort_by_field, DEFAULT_ORDER_BY)
+        order_by = filters.get("order_by") or DEFAULT_ORDER_BY
+        order_by_rule = getattr(sort_by_field, order_by, default_order_rule)
+
+        query = query.order_by(order_by_rule())
 
         return query
 
